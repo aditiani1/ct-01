bcebd92a968a776498ad634695daa9dc21d0dcf5
aditiani1 <ani25singh@gmail.com>
2022-08-26 15:39:01 +0530

    last commit

--- q1.cpp
+++ q1.cpp
@@ -1,11 +1,56 @@
 // Name : Aditi Singh
 // Roll : 2010990814
-// Set no : 
-// Q : 
-#include <bits/stdc++.h>
+// Set no : 5
+// Q : 1
+
+#include <iostream> 
+#include <unordered_map>    //using stl
 using namespace std;
+ 
+void findMaxLenSubarray(int nums[], int n, int S)
+{
+ 
+    unordered_map<int, int> map;
+ 
+    map[0] = -1;
+    int length = 0;
+    int last_index = -1;
+    int target = 0;  //initial target
+    for (int i = 0; i < n; i++)   //traversing the given array
+    {
+        target += nums[i];     // sum of elements 
+   
+        if (map.find(target) == map.end()) {
+            map[target] = i;
+        }
+ 
+        if (map.find(target - S) != map.end() && length < i - map[target - S])
+        {
+            length = i - map[target - S];
+            last_index = i;
+        }
+    }
+     
+    // print function for subarray
+    cout<<"The longest subarray is ";
+    cout<<"{";
+    for(int i=(last_index - length + 1);i<=last_index;i++)
+    {
+        cout<<nums[i]<<",";
+    }
+    cout<<"}";
+    cout<<" having length "<<(last_index - (last_index - length + 1) +1);
+}
+ 
+int main()
+{
+    int nums[] = { 5, 6, -5, 5, 3, 5, 3, -2, 0 };
+    int target = 8;
+	cout<<"Subarrays with sum "<<target<<" are : "<<endl;
 
-int main() {
-
+    int n = sizeof(nums) / sizeof(nums[0]); //size of array
+ 
+    findMaxLenSubarray(nums, n, target);  //calling the max subarray
+ 
     return 0;
 }
--- q2.cpp
+++ q2.cpp
@@ -1,11 +1,44 @@
 // Name : Aditi Singh
 // Roll : 2010990814
-// Set no : 
-// Q : 
-#include <bits/stdc++.h>
+// Set no : 5
+// Q : 2
+
+#include <iostream>
+#include <unordered_map>
 using namespace std;
+ 
+// Function to print all subarrays 
+void printAllSubarrays(int num[], int n)
+{
+    
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            // sum of elements in the array
+            sum = sum + num[j];
 
-int main() {
+            if (sum == 0) {
+                // cout << " subarrays with zero sum are : ";
+                cout<<"{";
+                for(int p=i; p<=j; p++)
+                {
+                    cout << num[p] <<",";
+                }
+                
+                cout << "}" <<endl;
+                 }
+        }
+    }
+}
+ 
+int main()
+{
+    cout<<"Subarrays with zero-sum are: "<<endl;
 
+    int num[] = { 4, 2, -3, -1, 0, 4};   //input 
+    int n = sizeof(num)/sizeof(num[0]);  //size of the array
+    printAllSubarrays(num, n);   //calling the print function
     return 0;
 }
